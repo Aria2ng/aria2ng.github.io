bc10a1aa3c4997cb956c455b4d906606fadfd003
urain39 <urain39@localhost>
2019-10-12 22:41:21 +0800

    update update.sh .

--- update.sh
+++ update.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 BASE_URL="https://github.com/mayswind/AriaNg"
-LATEST_TAG=$(curl -L -# -C - "${BASE_URL}/releases/latest" | sed -En 's/.+\/tag\/([.0-9A-Za-z]+)".+/\1/p')
+LATEST_TAG=$(curl -s "${BASE_URL}/releases/latest" | sed -En 's/.+\/tag\/([.0-9A-Za-z]+)".+/\1/p')
 
 # https://github.com/mayswind/AriaNg/releases/download/1.1.4/AriaNg-1.1.4-AllInOne.zip
-curl -L -# -C - "${BASE_URL}/releases/download/${LATEST_TAG}/AriaNg-${LATEST_TAG}-AllInOne.zip" -o new.zip 
+curl -L -# -C - "${BASE_URL}/releases/download/${LATEST_TAG}/AriaNg-${LATEST_TAG}-AllInOne.zip" -o new.zip
 
-unzip new.zip && rm -f new.zip
+unzip -o new.zip && rm -f new.zip
 
-
+# Enable insecure protocol
+sed -Ei 's/"https"===[0-9A-Za-z]+\.protocol\(\)/ false/g' index.html

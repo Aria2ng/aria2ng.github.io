03ee10f7de7e3f7c56c040712870308982909c43
urain39 <urain39@qq.com>
2021-11-10 19:32:34 +0800

    update Wed Nov 10 19:32:34 CST 2021

---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+set -e
+
+# Clone source and patches
+[ ! -d source ] && git clone git@github.com:mayswind/AriaNg.git source
+[ ! -d patches ] && git clone git@github.com:aria2ng/patches.git patches
+
+# Apply patches
+cd source
+for p in ../patches/*.patch; do
+  git am "$p"
+done
+
+# Build
+if [ ! -d node_modules ]; then
+  npm install
+fi
+./node_modules/gulp/bin/gulp.js process-assets-bundle
+
+# Replace
+cd ..
+cp source/dist/index.html .
+
+# Publish
+git add index.html update.sh
+git commit -m "update $(date)"
+git push origin master
+
